946b8eca9567b9cfc888bdc8783ccd64bd8f501a
minh quan <quan@opsify.dev>
2022-06-16 21:19:05 +0700

    Initial commit

---
+++ test/hello-ckd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HelloCkd from '../lib/hello-ckd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-ckd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HelloCkd.HelloCkdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
