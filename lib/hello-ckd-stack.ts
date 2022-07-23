0bf5e46fb369ad3fa5e87db63251ee6a912a62da
minh quan <quan@opsify.dev>
2022-07-23 19:38:30 +0700

    update demo

--- lib/hello-ckd-stack.ts
+++ lib/hello-ckd-stack.ts
@@ -6,14 +6,21 @@ export class HelloCkdStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-    new ec2.CfnVPC(this, 'vpc', {
-      cidrBlock: '10.0.0.0/24',
-      tags: [
-        {
-            key: `Name`,
-            value: `demo-vpc`
-        },
-      ],
-    });
-  }
+        const config = getConfig(scope);
+
+        // VPC
+        const vpc = new Vpc(this, config.projectName, config.environment, `network`);
+
+        // Subnet
+        new SN(this, config.projectName, config.environment, `network`, vpc);
+
+        // Internet Gateway
+        const internetGateway = new IGW(
+            this,
+            config.projectName,
+            config.environment,
+            `network`,
+            vpc,
+        );
+    }
 }
--- test/hello-ckd.test.ts
+++ test/hello-ckd.test.ts
@@ -20,29 +20,8 @@ const app = new cdk.App({
 
 const mainStack = new HelloCkdStack(app, `CdkStarterStack`, { env });
 
-<<<<<<< refs/remotes/origin/main
-test('snapshot has not changed', () => {
-    expect(Template.fromStack(mainStack)).toMatchSnapshot();
-});
-test('VPC Exists with correct CIDR and Tags', () => {
-    expect(
-        Template.fromStack(mainStack).hasResourceProperties('AWS::EC2::VPC', {
-            CidrBlock: '10.90.16.0/22',
-            Tags: [
-                {
-                    Key: 'Name',
-                    Value: 'cdk-learning-dev-network-vpc',
-                },
-            ],
-        }),
-    );
-});
-test('total 26 subnet', () => {
-    expect(Template.fromStack(mainStack).resourceCountIs('AWS::EC2::Subnet', 26));
-=======
 describe('MainStack', () => {
     test('snapshot MainStack', () => {
         expect(Template.fromStack(mainStack)).toMatchSnapshot();
     });
->>>>>>> 6.igw
 });
